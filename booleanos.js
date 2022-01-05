ca68ab3fe58e8ca652f97f427ace9ccf00fe46fd
SugoiSuki <niloeleuterio@gmail.com>
2022-01-04 23:12:29 -0300

    04.01 - 23:12

---
+++ booleanos.js
@@ -0,0 +1,17 @@
+let isAtivo = false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo);
+
+isAtivo = 1;
+console.log(!isAtivo);
+
+console.log('os verdadeiros...');
+console.log(!!0);
+console.log(!!-1);
+console.log(!!"");
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isAtivo = true));
